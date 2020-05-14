adb6da26f53f5079ff0e00c497edcb122d678b0f
Vadim Karpov <ksvkvv1990@gmail.com>
2020-05-14 10:49:01 +0300

    Initial commit

---
+++ src/main/java/koschei/models/Needle7.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Needle7 {
+    @Autowired
+    Deth8 deth8;
+
+    @Override
+    public String toString() {
+        return ", смерть Кощея на игле :( " + deth8.toString();
+    }
+}
---
+++ src/main/java/koschei/models/Rabbit4.java
@@ -0,0 +1,15 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Rabbit4 {
+    @Autowired
+    Duck5 duck5;
+
+    @Override
+    public String toString() {
+        return ", в зайце утка " + duck5.toString();
+    }
+}
